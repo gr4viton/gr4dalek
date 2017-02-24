7376798ad12ca7cc5cb5489a1d9b45729bbd0b8b
gr4viton <lordmutty@gmail.com>
2017-02-24 19:38:22 +0000

    horizontal and vertical as normal buttons joined

--- py/main.py
+++ py/main.py
@@ -2,12 +2,11 @@ import sys
 
 class GamePadButton():
     
-    def __init__(self, address, kind, name, dir1=None, dir2=None):
+    def __init__(self, address, kind, abbreviation,name):
+        self.abbr = abbreviation
         self.name = name
         self.add = address
         self.kind = kind
-        if dir1 is not None:
-            self.dir = [int(dir1), int(dir2)]
         self.state = 0
         self.data = None
 
@@ -19,38 +18,36 @@ class GamePadButton():
         return ''.join([str(word) for word in ['type=', self.kind,
                         ', add= ',self.add,
                         ', name="',self.name,
-                        '", state= ', self.state]])
+                        '", state= ', self.state,
+                        ', data=', self.data]])
            
 class GamePadControler():
     def __init__(self):
         self.open()
         actions = \
 """
-1,0:b:A button
-1,1:b:B button
-1,3:b:X button
-1,4:b:Y button
-1,6:b:L1 button
-1,7:b:R1 button
-1,10:b:SELECT button
-1,11:b:START button
+1,0:b:A:A button
+1,1:b:B:B button
+1,3:b:X:X button
+1,4:b:Y:Y button
+1,6:b:L1:L1 button
+1,7:b:R1:R1 button
+1,10:b:select:SELECT button
+1,11:b:start:START button
 
-2,0:s:Left horizontal stick
-2,1:s:Left vertical stick
-2,2:s:Right horizontal stick
-2,3:s:Right vertical stick
+2,0:s:LHstick:Left horizontal stick
+2,1:s:LVstick:Left vertical stick
+2,2:s:RHstick:Right horizontal stick
+2,3:s:RVstick:Right vertical stick
 
-2,4:b:R2 button
-2,5:b:L2 button
+2,4:b:R2:R2 button
+2,5:b:L2:L2 button
 
-2,6,1,128:a:Left arrow
-2,6,255,127:a:Right arrow
-2,6,1,128:a:Up arrow
-2,7,255,127:a:Down arrow
-
+2,6:a:leftright:Horizontal arrows
+2,7:a:updown:Vertical arrows
 """
-        self.longer_addresses = [(2,6), (2,7)]
 
+        
         minimal_line = '1,1:b:A'
         x = [line for line in actions.split('\n')]
         x = [line.split(':') for line in x if len(line)>=len(minimal_line)]
@@ -60,6 +57,9 @@ class GamePadControler():
 
         self.btns = {key : GamePadButton(key, *value) 
                 for key, value in self.states.items()}
+
+        named = {btn.abbr : btn for btn in self.btns.values()}
+        self.btns.update(named)
 
 #        for key, value in self.states.items():
 #            print(key, value)
@@ -83,12 +83,11 @@ class GamePadControler():
         print('data', data)
         print('address', add)
         
-        if add in self.longer_addresses:
-            add = tuple(add) + tuple(data)
-
         if self.btns.get(add) is not None:
             self.btns[add].update(data)
             print(self.btns[add])
+
+        print(self.btns['updown'].state)
 
     def readData(self):
         action = []
