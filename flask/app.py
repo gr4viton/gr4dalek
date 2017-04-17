eb8594fd817b6b65e5cf6c48032012d919f4d53c
gr4viton <lordmutty@gmail.com>
2017-04-17 11:18:10 +0000

    flask added http://192.168.0.199/video_js

---
+++ flask/app.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+from flask import Flask, render_template, Response, request
+import datetime
+import time
+
+from camera import Camera 
+from nocache import nocache
+
+import requests
+
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+
+app = Flask(__name__)
+
+
+GPIO.setmode(GPIO.BCM)
+
+
+def gen(camera):
+    while True:
+        #time.sleep(0.1)
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+@nocache
+def video_feed():
+    return Response(gen(Camera()), 
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/video_js')
+def video_js():
+    return render_template('video_js.html')
+
+
+def get_time_str():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M")
+
+def get_random_name():
+    #chosen by a fair random choice from calendar
+    return 'Peter'
+
+@app.route('/')
+def index():
+    action = request.args.get('action')
+    if action is None:
+        action = ''
+    if action=='snapshot':
+        return Response(gen(Camera()), 
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+    now = datetime.datetime.now()
+    time_str = now.strftime("%Y-%m-%d %H:%M")
+    temp = {
+        'title' : 'HELLO',
+        'time': time_str,
+        'name': 'SUCKER',
+        'random_name' : 'your mum',
+    }
+    return render_template('index.html', **temp)
+
+
+
+@app.route('/name/<name>')
+def hello(name):
+    temp = {
+        'title' : 'HELLO',
+        'time': get_time_str(),
+        'name' : name,
+        'random_name' : get_random_name(),
+    }
+
+    return render_template('index.html', **temp)
+
+
+@app.route("/readPin/<pin>")
+def readPin(pin):
+    try:
+        GPIO.setup(int(pin), GPIO.IN)
+        if GPIO.input(int(pin)) == True:
+            response = "Pin number " + pin + " is high!"
+        else:
+            response = "Pin number " + pin + " is low!"
+    except:
+        response = "There was an error reading pin " + pin + "."
+
+    templateData = {
+        'title' : 'Status of Pin' + pin,
+        'response' : response
+    }
+
+    return render_template('pin.html', **templateData)  
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
---
+++ flask/camera.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import sys
+import os
+
+class Camera():
+    def __init__(self):
+        self.stream_path = '/home/pi/stream/out.jpg'
+        self.default_img_path = '/home/pi/stream/default.jpg'
+
+        self.default_frame = open(self.default_img_path, 'rb').read()
+        self.frame = self.default_frame
+
+        self.stream_dir = '/home/pi/stream/'
+
+        self.fifo_buff = self.stream_dir + 'jpg'
+        self.server_waits = self.stream_dir + 'server_waits'
+        self.client_wants = self.stream_dir + 'client_wants'
+
+
+    def get_frame(self):
+        client_wants = os.path.exists(self.client_wants)
+        server_waits = os.path.exists(self.server_waits)
+        if not server_waits:
+            if not client_wants:
+                os.mkfifo(self.client_wants)
+        else: 
+            # server waits
+            if os.path.exists(self.fifo_buff):
+                self.frame = open(self.fifo_buff, 'rb', 0).read()
+                if client_wants:
+                    os.remove(self.client_wants)
+                if server_waits:
+                    os.remove(self.server_waits)
+            else:
+                pass
+        return self.frame
+
+    def get_frame_disk(self):
+        new = open(self.stream_path, 'rb').read()
+        if new is None:
+            return self.default_frame
+        else:
+            self.frame = new
+            return self.frame
+
+
